192d1816f7bc891ac7775eaf792d53d5f0c47f7d
Yacine Kherbache <yacine.kherbache@neomi.immo>
2023-07-19 02:11:21 +0200

    config: final fixes for the new back configs setup

--- app/back/config/linkFiles.sh
+++ app/back/config/linkFiles.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Loop through each subdirectory in the current directory
-for dir in ./*/; do
-	# Loop through each file in the subdirectory
-	for file in ${dir}*; do
-		# Create symbolic link in the parent directory
-		ln -s $(pwd)/${file} ../$(basename ${file})
-	done
+# Get a list of all directories within the current directory
+subdirs=(*/)
+for dir in "${subdirs[@]}"; do
+    files=$(find "$dir" -maxdepth 1 -type f)  # Find only files directly in the directory
+    for file in $files; do
+        filename=$(basename "$file")  # Extract the file name
+        ln -s "$(pwd)/${dir}${filename}" "../${filename}"  # Create the symbolic link in the root directory
+    done
 done
